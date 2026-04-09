5fec12567d5fc58f58e10fefdacb86f81e30f428
menaha <139383385+menahaha@users.noreply.github.com>
2026-04-09 09:56:53 +0530

    Add files via upload

---
+++ UC3.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        HashSet<String> bogieIDs = new HashSet<>();
+
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101");
+
+        System.out.println("Unique Bogie IDs:");
+        System.out.println(bogieIDs);
+    }
+}
